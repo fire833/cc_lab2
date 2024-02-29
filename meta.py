f02429e159bd18598144e53c7b7fb1733a5e4686
fire833 <kttpsy@gmail.com>
2024-02-29 11:14:42 -0600

    feat: initial commit

---
+++ meta.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from commands.commands import new_parser, runner
+
+def main():
+	p = new_parser()
+	opts = p.parse_args()
+	return runner(opts)
+
+# Entrypoint to all the things
+if __name__ == '__main__':
+	main()
