a035ebb03f048ec6a0e26956403ee535bce32cc5
fire833 <kttpsy@gmail.com>
2024-03-16 16:59:57 -0500

    feat(bruteforcer): crap is currently broken, wip

--- bruteforcer/src/optimize.rs
+++ bruteforcer/src/optimize.rs
@@ -10,6 +10,23 @@ impl SingleInstruction {
     const fn new(index: u32, value: u32) -> Self {
         Self { index, value }
     }
+
+    fn add_value_to_set(&self, set: &mut HashSet<u32>) {
+        set.insert(self.value);
+    }
+
+    fn is_key_in_set(&self, set: &mut HashSet<u32>) -> bool {
+        set.contains(&self.index)
+    }
+}
+
+impl From<(u32, u32)> for SingleInstruction {
+    fn from(value: (u32, u32)) -> Self {
+        Self {
+            index: value.0,
+            value: value.1,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -36,14 +53,10 @@ impl FourInstruction {
     }
 }
 
-impl Into<Vec<u32>> for FourInstruction {
-    fn into(self) -> Vec<u32> {
-        return vec![
-            self.value1.value,
-            self.value2.value,
-            self.value3.value,
-            self.value4.value,
-        ];
+// Returns the pairing of index to mapped value for each index.
+impl Into<Vec<SingleInstruction>> for FourInstruction {
+    fn into(self) -> Vec<SingleInstruction> {
+        return vec![self.value1, self.value2, self.value3, self.value4];
     }
 }
 
@@ -59,10 +72,10 @@ impl EightInstruction {
     }
 }
 
-impl Into<Vec<u32>> for EightInstruction {
-    fn into(self) -> Vec<u32> {
-        let mut vec1: Vec<u32> = self.value1.into();
-        let mut vec2: Vec<u32> = self.value2.into();
+impl Into<Vec<SingleInstruction>> for EightInstruction {
+    fn into(self) -> Vec<SingleInstruction> {
+        let mut vec1: Vec<SingleInstruction> = self.value1.into();
+        let mut vec2: Vec<SingleInstruction> = self.value2.into();
         vec1.append(&mut vec2);
 
         return vec1;
@@ -81,10 +94,10 @@ impl SixteenInstruction {
     }
 }
 
-impl Into<Vec<u32>> for SixteenInstruction {
-    fn into(self) -> Vec<u32> {
-        let mut vec1: Vec<u32> = self.value1.into();
-        let mut vec2: Vec<u32> = self.value2.into();
+impl Into<Vec<SingleInstruction>> for SixteenInstruction {
+    fn into(self) -> Vec<SingleInstruction> {
+        let mut vec1: Vec<SingleInstruction> = self.value1.into();
+        let mut vec2: Vec<SingleInstruction> = self.value2.into();
         vec1.append(&mut vec2);
 
         return vec1;
@@ -97,6 +110,17 @@ pub enum InstructionBlock {
     Four(FourInstruction),
     Eight(EightInstruction),
     Sixteen(SixteenInstruction),
+}
+
+impl Into<Vec<SingleInstruction>> for InstructionBlock {
+    fn into(self) -> Vec<SingleInstruction> {
+        match self {
+            InstructionBlock::Single(i) => return vec![i],
+            InstructionBlock::Four(i) => return i.into(),
+            InstructionBlock::Eight(i) => return i.into(),
+            InstructionBlock::Sixteen(i) => return i.into(),
+        }
+    }
 }
 
 /// A shiftmask wrapper struct.
@@ -112,71 +136,81 @@ impl ShiftMask {
 
     fn optimize_to_blocks(&self) -> Vec<InstructionBlock> {
         // check through all 4 blocks
-        let mut set: Vec<InstructionBlock> = vec![];
-        let mut refset: Vec<InstructionBlock>;
+        let mut set1: Vec<InstructionBlock> = vec![];
+        let mut set2: Vec<InstructionBlock> = vec![];
 
+        // only allocate one of these to maybe save on some space.
         let mut check: HashSet<u32> = HashSet::new();
+        let mut store_vec: Vec<SingleInstruction> = vec![];
 
         for (index, val) in self.values.iter().enumerate() {
-            set.push(InstructionBlock::Single(SingleInstruction::new(
+            set1.push(InstructionBlock::Single(SingleInstruction::new(
                 index as u32,
                 *val,
             )));
         }
 
+        let mut output: &Vec<InstructionBlock> = &vec![];
+
         for simd_count in vec![4, 8, 16].iter() {
-            let mut changed: bool = true;
+            let mut i = 0;
 
-            while changed {
-                changed = false;
-                refset = set.clone();
-                for (index, _) in refset.iter().rev().skip(simd_count - 1).rev().enumerate() {
-                    // If we have a self-permutation, then we can reduce into a new instruction.
-                    if self.range_self_permutes(
-                        &mut check,
-                        index as u32,
-                        index as u32 + (*simd_count as u32),
-                    ) {
-                        // if *simd_count == 4 {
-                        //     set.insert(index, InstructionBlock::Four(self.values[index]));
-                        // } else if *simd_count == 8 {
-                        //     set.insert(index, InstructionBlock::Eight(self.values[index]));
-                        // } else {
-                        //     set.insert(index, InstructionBlock::Sixteen(self.values[index]));
-                        // }
+            while i <= set1.len() - *simd_count {
+                if ShiftMask::chunk_self_permutes(
+                    &mut check,
+                    &mut store_vec,
+                    &set1.as_slice()[i..i + *simd_count],
+                ) {
+                    // match *simd_count {
+                    //     4 => set2.push(InstructionBlock::Four(FourInstruction::new(
+                    //         set1[i],
+                    //         set1[i + 1],
+                    //         set1[i + 2],
+                    //         set1[i + 3],
+                    //     ))),
+                    //     8 => {}
+                    //     16 => {}
+                    //     _ => {}
+                    // }
 
-                        for _ in 0..(*simd_count as u32) {
-                            set.remove(index + 1);
-                        }
-
-                        changed = true;
-
-                        break;
-                    }
+                    i += *simd_count;
+                } else {
+                    set2.push(set1[i]);
+                    i += 1;
                 }
             }
+
+            let tmp = set1.clone();
+            set1 = set2;
+            set2 = tmp;
+            output = &set2;
         }
 
-        set
+        output.to_vec()
     }
 
     // Returns whether a range of elements "self permutes" on
     // themselves, I.e. the array subset that can be optimized
     // to a single SIMD instruction.
-    fn range_self_permutes(&self, set: &mut HashSet<u32>, lower: u32, upper: u32) -> bool {
+    fn chunk_self_permutes(
+        set: &mut HashSet<u32>,
+        full_vec: &mut Vec<SingleInstruction>,
+        chunk: &[InstructionBlock],
+    ) -> bool {
         set.clear();
+        full_vec.clear();
 
-        // If we indices that are out of bounds, bail out.
-        if upper > (self.values.len() - 1) as u32 {
-            return false;
+        for blk in chunk.iter() {
+            let mut cloned: Vec<SingleInstruction> = blk.clone().into();
+            full_vec.append(&mut cloned);
         }
 
-        for i in lower..upper {
-            set.insert(i);
+        for instr in full_vec.iter() {
+            instr.add_value_to_set(set);
         }
 
-        for i in lower..upper {
-            if !set.contains(&self.values[i as usize]) {
+        for instr in full_vec.iter() {
+            if !instr.is_key_in_set(set) {
                 return false;
             }
         }
@@ -188,8 +222,8 @@ impl ShiftMask {
 #[test]
 fn test_dp() {
     let mask = ShiftMask::new(vec![1, 2, 3, 0, 4, 7, 5, 6]);
-    assert_eq!(
-        mask.optimize_to_blocks(),
-        vec![InstructionBlock::Eight(1, 2, 3, 0, 4, 7, 5, 6)]
-    );
+    // assert_eq!(
+    //     mask.optimize_to_blocks(),
+    //     vec![InstructionBlock::Eight(1, 2, 3, 0, 4, 7, 5, 6)]
+    // );
 }
