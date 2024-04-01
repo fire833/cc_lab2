15fe6aa26a93d21c4ac7edd574456299d9ed35c6
fire833 <kttpsy@gmail.com>
2024-03-31 22:12:21 -0500

    Revert "added --auto_args which will automatically create an input for you if you want to run the program once. added --sequence_amount which lets you make sure theres a set amount of sequences in the array (make sure the array length is big enough)"
    This reverts commit 0c366dcefe9b2cf10d09dc8217b2d7c85e01f0ee.

--- commands.py
+++ commands.py
@@ -33,8 +33,6 @@ def new_parser():
 	rgen.add_argument("--template", help="Provide the template you want to generate from", type=str, default="base2", dest="template")
 	rgen.add_argument("--output", help="Provide the output location of the generated program executable.", type=str, default=progOut, dest="output")
 	rgen.add_argument("--arg_count", help="Provide the number of arguments to build with your random permutation.", type=int, default=1000, dest="arg_count")
-	rgen.add_argument("--auto_args", help="Automatically input the amount of args needed", action="store_true", dest="auto_args", default=False)
-	rgen.add_argument("--sequence_amount", help="format: number of 8 sequences, number of 4 sequences", type=str, default=",", dest="sequence_amount")
 	rgen.set_defaults(func=generate_rand)
 	r = sub.add_parser("run")
 	r.add_argument("--values", help="Provide a comma-separated list of integers to pass to the generated program.", type=str, dest="values")
@@ -56,7 +54,7 @@ def runner(args: ArgumentParser):
 		print("generating new output program")
 		return generate(args.pattern, args.template, args.output, False, False)
 	elif sys.argv[1] == "genrand":
-		return generate_rand(args.template, args.output, args.arg_count, False, False, args.auto_args, args.sequence_amount)
+		return generate_rand(args.template, args.output, args.arg_count, False, False)
 	elif sys.argv[1] == "run":
 		print("running output program")
 		return run(args.input, [int(arg) for arg in args.values.split(",")])
@@ -82,34 +80,21 @@ templates = {
 }
 
 def generate(pattern: str, template: str, output: str, asm: bool, omp: bool):
-	
-	#the code looks a little more goofy but it works like this
-	if pattern.find(';') != 1: #check if theres a second half (an automatically generated input)
-		parts = pattern.split(';')
-		second_half = parts[1].split(',') if len(parts) > 1 else []
-		inputlen = len(second_half)
-		second_half = ','.join(map(str, second_half))
-		values: [(int, int)] = [(i, int(arg)) for i, arg in enumerate(parts[0].split(","))]
-		pattern = parts[0]
-	else: 
-		values: [(int, int)] = [(i, int(arg)) for i, arg in enumerate(pattern.split(","))]
+	values: [(int, int)] = [(i, int(arg)) for i, arg in enumerate(pattern.split(","))]
 	arg_count = len(values)
 
 	outfile = templates[template]["program_output"]
-	
+
 	permute_gen = ""
 	if template == "simd1" or template == "simd2":
 		print("generating simd optimizations with bruteforcer")
 		permute_gen = subprocess.run(["cargo", "run", "--manifest-path=bruteforcer/Cargo.toml", "--", "-p", pattern, "simplec"], capture_output=True).stdout.decode()
-	
+
 	print("rendering template")
 	env = jinja2.Environment()
 	t = env.from_string(templates[template]["template"])
 	f = open(file=outfile, mode="w")
-	if 'second_half' in locals():
-		f.write(t.render(arg_count=arg_count, values=values, permute_gen=permute_gen, inputstr=second_half))
-	else:
-		f.write(t.render(arg_count=arg_count, values=values, permute_gen=permute_gen))
+	f.write(t.render(arg_count=arg_count, values=values, permute_gen=permute_gen))
 	f.close()
 
 	args = templates[template]["compiler_prefix"]
@@ -132,20 +117,10 @@ def rand_pattern(arg_count: int):
 	random.shuffle(nums)
 	return nums
 
-def generate_rand(template: str, output: str, arg_count: int, asm: bool, omp: bool, auto_args: bool, sequence_amount: str):
+def generate_rand(template: str, output: str, arg_count: int, asm: bool, omp: bool):
 	numstr = ""
-	if (sequence_amount != ","):
-		sequences = sequence_amount.split(",")
-		numstr += generate_sequences(100, int(sequences[0]), int(sequences[1]))
-	else:
-		for i in rand_pattern(arg_count):
-			numstr += f"{i},"
-		numstr = numstr.removesuffix(",")
-
-	numstr += ";"
-	if auto_args:
-		for i in list(range(1, arg_count+1)):
-			numstr += f"{i},"
+	for i in rand_pattern(arg_count):
+		numstr += f"{i},"
 	numstr = numstr.removesuffix(",")
 
 	print(f"pattern: {numstr}")
@@ -226,39 +201,3 @@ def run_tests(tmplversions: [str]):
 					print(f"test {i} passed")
 				else:
 					print(f"test {i} failed, comparison inequality")
-
-#Generate an array depending on your desired number of sequences of 8/4 numbers (may mess up on smaller input sizes)
-def generate_sequences(length, num_sequences_8, num_sequences_4):
-    array = list(range(1, length + 1))
-    
-    sequences_8 = []
-    sequences_4 = []
-    
-    for i in range(num_sequences_8):
-        start_index = random.randint(0, len(array) - 8)
-        seq = array[start_index:start_index+8]
-        random.shuffle(seq)
-        sequences_8.append(seq)
-
-        array = [x for x in array if x not in seq]
-    
-    for i in range(num_sequences_4):
-        start_index = random.randint(0, len(array) - 4)
-        seq = array[start_index:start_index+4]
-        random.shuffle(seq)
-        sequences_4.append(seq)
-        array = [x for x in array if x not in seq]
-	
-    print(sequences_4)
-    print(sequences_8)
-    random.shuffle(array)
-
-    shuffled_array = []
-    for num in array:
-        shuffled_array.append(str(num))
-    
-    for seq in sequences_8 + sequences_4:
-        insert_index = random.randint(0, len(shuffled_array))
-        shuffled_array[insert_index:insert_index] = map(str, seq)  # Convert numbers to strings
-        
-    return ','.join(shuffled_array)
