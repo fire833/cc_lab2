08d9b6bed01ab5a798c242404c07ae8b17379b7c
fire833 <kttpsy@gmail.com>
2024-04-02 16:14:27 -0500

    feat: more fixing crap

--- commands.py
+++ commands.py
@@ -29,11 +29,13 @@ def new_parser():
 	gen = sub.add_parser("generate")
 	gen.add_argument("--pattern", help="Provide the pattern you want to generate against (should be a string with spaces)", type=str, dest="pattern")
 	gen.add_argument("--template", help="Provide the template you want to generate from", type=str, default="base1", dest="template")
-	gen.add_argument("--output", help="Provide the output location of the generated program executable.", type=str, default=progOut, dest="output")
+	gen.add_argument("--bin-output", help="Provide the output location of the generated program executable.", type=str, default="./prog", dest="binout")
+	gen.add_argument("--output", help="Provide the output location of the generated program source.", type=str, default="", dest="output")
 	gen.set_defaults(func=generate)
 	rgen = sub.add_parser("genrand")
 	rgen.add_argument("--template", help="Provide the template you want to generate from", type=str, default="base2", dest="template")
-	rgen.add_argument("--output", help="Provide the output location of the generated program executable.", type=str, default=progOut, dest="output")
+	rgen.add_argument("--bin-output", help="Provide the output location of the generated program executable.", type=str, default="./prog", dest="binout")
+	rgen.add_argument("--output", help="Provide the output location of the generated program source.", type=str, default="", dest="output")
 	rgen.add_argument("--arg_count", help="Provide the number of arguments to build with your random permutation.", type=int, default=1000, dest="arg_count")
 	rgen.set_defaults(func=generate_rand)
 	r = sub.add_parser("run")
@@ -55,9 +57,9 @@ def runner(args: ArgumentParser):
 	# print(args)
 	if sys.argv[1] == "generate":
 		print("generating new output program")
-		return generate(args.pattern, args.template, args.output, False, False)
+		return generate(args.pattern, args.template, args.binout, args.output, False, False)
 	elif sys.argv[1] == "genrand":
-		return generate_rand(args.template, args.output, args.arg_count, False, False)
+		return generate_rand(args.template, args.binout, args.output, args.arg_count, False, False)
 	elif sys.argv[1] == "run":
 		print("running output program")
 		return run(args.input, [int(arg) for arg in args.values.split(",")])
@@ -88,11 +90,12 @@ benchplates = {
 	"simd2": tmplsimd2,
 }
 
-def generate(pattern: str, template: str, output: str, asm: bool, omp: bool):
+def generate(pattern: str, template: str, binout: str, output: str, asm: bool, omp: bool):
 	values: [(int, int)] = [(i, int(arg)) for i, arg in enumerate(pattern.split(","))]
 	arg_count = len(values)
 
-	outfile = templates[template]["program_output"]
+	if output == "":
+		output = os.path.join(os.path.dirname(binout), templates[template]["program_output"])
 
 	permute_gen = ""
 	if template == "simd1" or template == "simd2":
@@ -106,13 +109,13 @@ def generate(pattern: str, template: str, output: str, asm: bool, omp: bool):
 	print("rendering template")
 	env = jinja2.Environment()
 	t = env.from_string(templates[template]["template"])
-	f = open(file=outfile, mode="w")
+	f = open(file=output, mode="w")
 	f.write(t.render(arg_count=arg_count, values=values, permute_gen=permute_gen))
 	f.close()
 
 	args = templates[template]["compiler_prefix"]
 	args.append("-o")
-	args.append(output)
+	args.append(binout)
 	if omp:
 		args.append("-fopenmp")
 	if asm:
@@ -121,7 +124,7 @@ def generate(pattern: str, template: str, output: str, asm: bool, omp: bool):
 		args.pop(len(args) - 1)
 		args.pop(len(args) - 1)
 
-	args.append(outfile)
+	args.append(output)
 	print("compiling program")
 	subprocess.run(args)
 
@@ -130,11 +133,11 @@ def rand_pattern(arg_count: int):
 	random.shuffle(nums)
 	return nums
 
-def generate_rand(template: str, output: str, arg_count: int, asm: bool, omp: bool):
+def generate_rand(template: str, binout: str, output: str, arg_count: int, asm: bool, omp: bool):
 	numstr = subprocess.run(["cargo", "run", "--manifest-path=bruteforcer/Cargo.toml", "--", "-l", str(arg_count), "randpat"], capture_output=True).stdout.decode()
 	numstr = numstr.strip("\n")
 	print(f"pattern: {numstr}")
-	generate(numstr, template, output, asm, omp)
+	generate(numstr, template, binout, output, asm, omp)
 
 def run(input: str, args: [int]):
 	parsed = ""
@@ -166,28 +169,12 @@ def bench(outDir: str, outFile: str):
 	for name, tmpl in benchplates.items():
 		for arg_count, patternset in patterns.items():
 			for i, pattern in enumerate(patternset):
-				print(f"running template {name} arg_count {arg_count} pattern {i}...")
 				pname = name + "_" + str(arg_count) + "_" + str(i) + "_" + tmpl["program_output"]
-				outStr = f"{outDir}/{name}"
-				generate(pattern, name, outStr, False, False)
-				values = run_rand(outStr, 2500, arg_count)
-
-def run_report(patterns: [(str, int)], tmplversions: [str], output: str):
-	for tmpl in tmplversions:
-		for pattern in patterns:
-			pat = pattern[0].replace(" ", "_").replace(",", "-")
-			out = f"{output}/{tmpl}_{pat}"
-
-			for i in range(2):
-				doOpenMP = False
-				csvfile = f"{output}/{tmpl}_{pat}.csv"
-				if i == 1:
-					csvfile = f"{output}/{tmpl}_{pat}_omp.csv"
-					doOpenMP = True
-
-				generate(pattern[0], tmpl, out + ".c", out, "gcc", False, doOpenMP, 1000)
-				outputs = run_rand(out, 250, pattern[1], 20000)
-				write_csv(outputs, csvfile)
+				output = f"{outDir}/{pname}"
+				outbin = f"{outDir}/prog"
+				print(f"running template {name} arg_count {arg_count} pattern {i} at {output}...")
+				generate(pattern, name, outbin, output, False, False)
+				values = run_rand(outbin, 2500, arg_count)
 
 def write_csv(outputs, output: str):
 	f = open(output, "w")
